d2e7668ce3a80801760fa41ded2e41f4946f25e7
Papillard <boris.paillard@gmail.com>
2013-12-29 15:36:47 +0100

    Update provider_grouping.rb

--- Wagon-3/02. Provider grouping/provider_grouping.rb
+++ Wagon-3/02. Provider grouping/provider_grouping.rb
@@ -1,6 +1,5 @@
 def group_mails( emails ) 
   # your code goes here
-  emails_from = {}
 end
 
 users_emails = %w( bob@yahoo.fr 
@@ -11,28 +10,10 @@ users_emails = %w( bob@yahoo.fr
                    monsieur.mack@gmail.com )
                    
 
-first_grouping = group_mails( users_emails ) 
-
-puts first_grouping 
+puts group_mails( users_emails ) 
 
 # => {"yahoo" => ["bob@yahoo.fr", "bigbox@yahoo.fr"], 
 #     "hotmail" => ["roger57@hotmail.fr"], 
 #     "lewagon" => ["boris@lewagon.org"], 
 #     "gmail" => ["monsieur.olivier@gmail.com", 
 #     "monsieur.mack@gmail.com"]}
-
-
-# Why Symbols ?
-new_emails = %w( bob@yahoo.fr 
-                 roger57@hotmail.fr 
-                 bigbox@yahoo.fr 
-                 boris@lewagon.org 
-                 monsieur.olivier@gmail.com 
-                 monsieur.mack@gmail.com
-                 monsieur.romain@gmail.com )
-                  
-                   
-second_grouping = group_mails( new_emails ) 
-
-puts first_grouping.keys.map { |provider| provider.object_id } 
-puts second_grouping.keys.map { |provider| provider.object_id }
