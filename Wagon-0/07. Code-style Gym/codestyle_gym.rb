74ef599f81176a745ac72994c3c764da026fc6d7
Benoit Chibrac <benoitchibrac@gmail.com>
2014-01-07 15:54:31 +0100

    codestyle_gym

--- Wagon-0/07. Code-style Gym/codestyle_gym.rb
+++ Wagon-0/07. Code-style Gym/codestyle_gym.rb
@@ -1,13 +1,16 @@
-class Player_moving
-  Grid_POSITIONS = (1..100).to_a
+class [Player_moving] 
+  GRID_POSITIONS = (1..100).to_a
   
-def initialize ( position ) 
-  @position = position ; @iterations = 0; 
-raise "Position must be between 1 and 100" unless Grid_POSITIONS.include? @position
-  end
+def initialize (position) 
+  @position = position ; 
+  @iterations = 0; 
+  raise "Position must be between 1 and 100" 
+  unless GRID_POSITIONS.include? @position
+
+end
   
-      def move   distanceInMeters
-@position += distanceInMeters
+def move (distance_in_meters)
+@position += "distanceInMeters"
   end
   
 def play
@@ -24,7 +27,7 @@ def cheat; @position = 101; end
 def status_Player
     if( @position >   100 )
       :win 
-    elsif(@position<0) ; :loose
+    elsif( @position<0 ) ; :loose
     else;  :play
     end
   end
@@ -33,11 +36,11 @@ def status_Player
     
 end
 
-player = Player_moving.new(50)
-player.play
-puts player.status_Player == :play # true
-player.cheat
-puts player.hasWon == true
+  player = Player_moving.new(50)
+  player.play
+  puts player.status_Player == :play # true
+  player.cheat
+  puts player.hasWon == true
   
 begin
   Player_moving.new(110)
