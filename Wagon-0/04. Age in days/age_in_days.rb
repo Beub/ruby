4ad263ff06e9bf3ea1528b022f8b4b4fe915a64b
Benoit Chibrac <benoitchibrac@gmail.com>
2014-01-07 10:47:12 +0100

    age_in_days.rb

--- Wagon-0/04. Age in days/age_in_days.rb
+++ Wagon-0/04. Age in days/age_in_days.rb
@@ -1,7 +1,11 @@
 def age_in_days(day, month, year)
   # your code goes here !
+
+ 	t0 = Time.now.to_i
+ 	t1 = Time.new(year, month, day).to_i
+ 	t2 = (t0-t1)/(3600.*24)
 end
 
 
 # Testing your code
-puts age_in_days(6, 11, 1985)
+puts age_in_days(10, 12, 1989)
