e5e7a3ee1c799a986bb51d2504464e450ad183af
Benoit Chibrac <benoitchibrac@gmail.com>
2014-01-07 11:29:09 +0100

    introduce_yourself

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,12 @@
 # your code goes here, introduce yourself !
+puts "Whats is your first name?"
+	first_name = gets.chomp
+
+puts "Whats is your middle name?"
+	middle_name = gets.chomp
+
+puts "Whats is your last name?"
+	Nom_de_famille = gets.chomp
+
+puts "Hello, #{first_name} #{middle_name} #{Nom_de_famille}"
+puts "Hello" + first_name + middle_name + Nom_de_famille
