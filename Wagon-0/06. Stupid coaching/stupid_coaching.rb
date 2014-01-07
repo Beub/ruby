7176c5e4bf905582e1d514f3c2794d4e8355a9d5
Benoit Chibrac <benoitchibrac@gmail.com>
2014-01-07 15:01:58 +0100

    stupid_coaching

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,25 @@
-def stupid_coaching 
-  # your code goes here
+def ask_user
+	gets.chomp
+end
+
+def coach_response(answer)
+  if answer.end_with?("?")
+		puts "silly question"
+  else
+		puts "I don't care son!"
+  end
+end
+
+def stupid_coaching
+
+	puts "what's up kid"
+
+	while true 
+		answer = ask_user
+		break if answer == "leave me alone"
+		coach_response(answer)
+	end
+  puts "Ok, you got it!"
 end
 
 # Calling the method
